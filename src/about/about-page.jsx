40ce3cc386555ae3debf2e8f027a916bd65b38ae
Baz <bk@thinkloop.com>
2016-10-04 19:40:38 +0000

    updated about page

--- src/about/about-page.jsx
+++ src/about/about-page.jsx
@@ -8,7 +8,21 @@ const AboutPage = (p) => (
 		<SiteHeader { ...p.siteHeader } />
 
 		<main className={ classnames('page', p.className) }>
-			See the article for more details: <a href="http://www.thinkloop.com/article/extreme-decoupling-react-redux-selectors" target="_blank">Extreme Decoupling React Redux Selectors</a>
+			<p>
+				Read the article for more details: <a href="http://www.thinkloop.com/article/extreme-decoupling-react-redux-selectors" target="_blank">Extreme Decoupling React Redux Selectors</a>
+			</p>
+
+			<p>
+				See the code on github:
+			</p>
+
+			<ul>
+				<li><a href="https://github.com/thinkloop/todo-app"><strong>integration</strong>: selectors</a></li>
+				<ul>
+					<li><a href="https://github.com/thinkloop/todo-react-components"><strong>view</strong>: react components</a></li>
+					<li><a href="https://github.com/thinkloop/todo-redux-state"><strong>state</strong>: redux container</a></li>
+				</ul>
+			</ul>
 		</main>
 	</div>
 );
